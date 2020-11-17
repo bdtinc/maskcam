d7643af4e4afaa57afa5f3b3e44f751b446e1d9a
DonBraulio <braulioriosf@gmail.com>
2020-11-17 18:32:39 -0300

    Debug setup, displaying raw detections

--- maskcam/run_yolo.py
+++ maskcam/run_yolo.py
@@ -5,16 +5,9 @@ import time
 
 from norfair.video import Video
 from norfair.tracker import Tracker
-from norfair.drawing import draw_tracked_objects, draw_points, draw_debug_metrics, Color
+from norfair.drawing import draw_tracked_objects, draw_debug_metrics, Color
 
 from face_mask_detector import FaceMaskDetector
-
-from integrations.yolo.detector_darknet import DetectorDarknet
-
-# from integrations.yolo.detector_pytorch import DetectorYoloPytorch
-
-# Required python tensorrt, usually compiled for python 3.6 at system level
-# from integrations.yolo.detector_trt import DetectorYoloTRT
 
 from integrations.yolo.yolo_adaptor import YoloAdaptor
 
@@ -24,12 +17,23 @@ with open("config.yml", "r") as stream:
     # Not using Loader=yaml.FullLoader since it doesn't work on jetson PyYAML version
     config = yaml.load(stream)
 
-# Yolo implementation to use
-detector = DetectorDarknet({**config["yolo_darknet"], **config["yolo_generic"]})
+# Pick YOLO detector implementation to use
+yolo_variant = config["yolo_generic"]["yolo_variant"]
+yolo_config = {**config[yolo_variant], **config["yolo_generic"]}
 
-# detector = DetectorYoloPytorch({**config["yolo_pytorch"], **config["yolo_generic"]})
+if yolo_variant == "yolo_trt":  # Fastest implementation
+    # Requires python tensorrt, usually compiled for python 3.6 at system level
+    from integrations.yolo.detector_trt import DetectorYoloTRT  # noqa
 
-# detector = DetectorYoloTRT({**config["yolo_trt"], **config["yolo_generic"]})
+    detector = DetectorYoloTRT(yolo_config)
+elif yolo_variant == "yolo_pytorch":
+    from integrations.yolo.detector_pytorch import DetectorYoloPytorch  # noqa
+
+    detector = DetectorYoloPytorch(yolo_config)
+else:
+    from integrations.yolo.detector_darknet import DetectorDarknet  # noqa
+
+    detector = DetectorDarknet(yolo_config)
 
 # Converter functions from Yolo -> Tracker + FaceMaskDetector
 pose_adaptor = YoloAdaptor(config["yolo_generic"])
@@ -62,6 +66,11 @@ timer_yolo = 0.0  # Reset to 0.0 after first frame to avoid counting model loadi
 timer_tracker = 0.0
 timer_logic = 0.0
 timer_drawing = 0.0
+
+# High level components
+tracker_enabled = config["debug"]["tracker_enabled"]
+facemask_enabled = config["debug"]["facemask_enabled"]
+
 for k, frame in enumerate(video):
 
     tick = time.time()
@@ -72,47 +81,54 @@ for k, frame in enumerate(video):
 
     # Tracker update
     tick = time.time()
-    tracked_people = tracker.update(
-        detections_tracker, period=config["general"]["inference_period"]
-    )
+    if tracker_enabled:
+        tracked_people = tracker.update(
+            detections_tracker, period=config["general"]["inference_period"]
+        )
     timer_tracker += time.time() - tick
 
     # Detect and classify faces from tracked poses
     tick = time.time()
-    boxes_face_ok, boxes_face_fail = face_mask_detector.detect_face_masks(frame, tracked_people)
+    if facemask_enabled and tracker_enabled:
+        boxes_face_ok, boxes_face_fail = face_mask_detector.detect_face_masks(
+            frame, tracked_people
+        )
     timer_logic += time.time() - tick
 
     # Drawing functions
     tick = time.time()
-    face_mask_detector.draw_margins(frame)
-    if config["general"]["draw_classification"]:
-        face_mask_detector.draw_classification(frame, tracked_people)
-    if config["debug"]["draw_detections"]:  # Using yolo detections
-        # draw_points(frame, detections_tracker)
+    if config["debug"]["draw_detections"]:  # Raw yolo detections
         pose_adaptor.draw_raw_detections(frame, detections_tracker)
-    if config["debug"]["draw_face_boxes"]:  # Using tracker
-        face_mask_detector.draw_face_boxes(frame, boxes_face_ok, boxes_face_fail)
-    if config["debug"]["draw_predictions"]:
-        draw_tracked_objects(frame, tracked_people, id_size=0)
-    if config["debug"]["draw_tracking_ids"]:
-        draw_tracked_objects(
-            frame, tracked_people, draw_points=False, id_thickness=1, color=Color.white
-        )
-    if config["debug"]["draw_tracking_debug"]:
-        draw_debug_metrics(frame, tracked_people)
 
-    # Side panel
-    panel_faces = config["general"]["draw_panel_faces"]
-    panel_text = config["general"]["draw_statistics_text"]
-    panel_graph = config["general"]["draw_statistics_graphics"]
-    if panel_faces or panel_text or panel_graph:
-        face_mask_detector.draw_panel_background(frame)
-        if panel_faces:
-            face_mask_detector.draw_panel_faces(frame, tracked_people)
-        if panel_text:
-            face_mask_detector.draw_statistics_text(frame)
-        if panel_graph:
-            face_mask_detector.draw_statistics_graphics(frame)
+    if tracker_enabled:
+        if config["debug"]["draw_predictions"]:
+            draw_tracked_objects(frame, tracked_people, id_size=0)
+        if config["debug"]["draw_tracking_ids"]:
+            draw_tracked_objects(
+                frame, tracked_people, draw_points=False, id_thickness=1, color=Color.white
+            )
+        if config["debug"]["draw_tracking_debug"]:
+            draw_debug_metrics(frame, tracked_people)
+
+    if facemask_enabled and tracker_enabled:
+        face_mask_detector.draw_margins(frame)
+        if config["general"]["draw_classification"] and facemask_enabled:
+            face_mask_detector.draw_classification(frame, tracked_people)
+        if config["debug"]["draw_face_boxes"]:  # Using tracker
+            face_mask_detector.draw_face_boxes(frame, boxes_face_ok, boxes_face_fail)
+
+        # Side panel
+        panel_faces = config["general"]["draw_panel_faces"]
+        panel_text = config["general"]["draw_statistics_text"]
+        panel_graph = config["general"]["draw_statistics_graphics"]
+        if panel_faces or panel_text or panel_graph:
+            face_mask_detector.draw_panel_background(frame)
+            if panel_faces:
+                face_mask_detector.draw_panel_faces(frame, tracked_people)
+            if panel_text:
+                face_mask_detector.draw_statistics_text(frame)
+            if panel_graph:
+                face_mask_detector.draw_statistics_graphics(frame)
 
     video.write(frame)
     timer_drawing += time.time() - tick
