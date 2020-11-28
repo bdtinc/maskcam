e2d0169e5001bb47a92989ab61eba67c5c1a3c68
Shehrzad <s.qureshi@saphotonics.com>
2020-11-27 21:36:51 -0800

    Creation (see https://github.com/enazoe/yolo-tensorrt)

---
+++ yolo-tensorrt/modules/class_detector.cpp
@@ -0,0 +1,36 @@
+#include "class_detector.h"
+#include "class_yolo_detector.hpp"
+
+class Detector::Impl
+{
+public:
+	Impl() {}
+
+	~Impl(){}
+
+	YoloDectector _detector;
+};
+
+Detector::Detector()
+{
+	_impl = new Impl();
+}
+
+Detector::~Detector()
+{
+	if (_impl)
+	{
+		delete _impl;
+		_impl = nullptr;
+	}
+}
+
+void Detector::init(const Config &config)
+{
+	_impl->_detector.init(config);
+}
+
+void Detector::detect(const std::vector<cv::Mat> &mat_image, std::vector<BatchResult> &vec_batch_result)
+{
+	_impl->_detector.detect(mat_image, vec_batch_result);
+}
